0c940cfdb2e319e674e4176cda638bfda15ee4d9
Sujeet Kumar Srivastava <sujeet@aleph-labs.com>
2018-05-24 17:38:10 +0800

    Duplicate counting solution

---
+++ DuplicateCount/solution.js
@@ -0,0 +1,9 @@
+function duplicateCount(text){
+    var dup = [];
+    text.toLowerCase().split('').forEach(function(v, i, arr) {
+        if(i != arr.lastIndexOf(v) && dup.indexOf(v) == -1){
+            dup.push(v);
+        }
+    });
+    return dup.length;
+}
